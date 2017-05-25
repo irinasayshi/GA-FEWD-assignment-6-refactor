e23430a64a1804971313f6d0d9802dae340d3165
irinasayshi <irinagorelik@Irinas-MacBook-Pro.local>
2017-05-24 18:43:40 -0600

    support for enter key; reset background

--- js/index.js
+++ js/index.js
@@ -4,25 +4,28 @@ $( document ).ready(function(){
 var tempF = 0; 
 
 // set blur function to run function when user clicks out of field
-	$("input").blur(function(){
+	$("form").on("submit", function(){
+		event.preventDefault();
     	var tempCelcius = $.trim($('#currentTemp').val()); // trim var to remove spaces
 		if (tempCelcius != '' && tempCelcius.match(/^\d+$/)){ // check if input is empty or not a number
 			tempCelcius = parseInt(tempCelcius); // convert string to a number 
     		tempF = Math.round((tempCelcius * 1.8) + 32); // apply conversion from C to F
     		$('.tempF').text(tempF); // output
+
+    		// change background code starts here
+
+	    	if(tempF > 85){ // change the background to a desert if the resulting temp is > 85 F
+				$('body').attr('class', 'hot');
+				// alert('Temp is over 85!');
+			} else if(tempF < 50){ // change to a snowy picture if < 50 degrees
+				$('body').attr('class', 'cold');
+			} else  if(tempF >= 50 && tempF <= 85){ // otherwise change background to a beautiful meadow picture because temp is 50-85 degrees
+				$('body').attr('class', 'nice');
+			} 
+
     	} else {
-    		alert('You need to enter a number!'); // otherwise if input is empty or not a number, alert the user
+    		$('.tempF').text('You need to enter a number!'); // otherwise if input is empty or not a number, alert the user
+    		$('body').attr('class', '');
     	}
-
-    	// change background code starts here
-
-    	if(tempF > 85){ // change the background to a desert if the resulting temp is > 85 F
-			$('body').attr('class', 'hot');
-			// alert('Temp is over 85!');
-		} else if(tempF < 50){ // change to a snowy picture if < 50 degrees
-			$('body').attr('class', 'cold');
-		} else { // otherwise change background to a beautiful meadow picture because temp is 50-85 degrees
-			$('body').attr('class', 'nice');
-		}
 	});
 });
